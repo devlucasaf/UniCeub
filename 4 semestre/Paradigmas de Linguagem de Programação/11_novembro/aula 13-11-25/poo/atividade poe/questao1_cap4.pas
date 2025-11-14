9d335f1cde512a57a99d37b506f432e6582c7068
beneficios computações <freitas.lucasaf@gmail.com>
2025-11-13 22:53:01 -0300

    Editando código em Pascal

--- 4 semestre/Paradigmas de Linguagem de Programação/11_novembro/aula 13-11-25/poo/atividade poe/questao1_cap4.pas
+++ 4 semestre/Paradigmas de Linguagem de Programação/11_novembro/aula 13-11-25/poo/atividade poe/questao1_cap4.pas
@@ -1,3 +1,24 @@
+{*
+Paradigmas de Linguagem de Programação
+Aula: 13-11-25
+Programação Orientada a Eventos
+Capítulo 4 - Livro Base sobre Paradigma Orientado a Eventos
+
+1. Marque V para Verdadeiro e F para Falso, considerando que as afirmações
+indicam eventos de acordo com o paradigma estudado neste capítulo:
+    ( ) Clicar com o botão esquerdo do mouse
+    ( ) Acessar uma tabela no banco de dados
+    ( ) Pressionar uma tecla sobre um controle
+    ( ) Mostrar um componente (controle) na tela
+    ( ) Entrar ou sair de um formulário (janela)
+
+2. Complete as lacunas:
+    Os controles, também podem ser chamados de componentes ou objetos.
+    Os controles possuem ________________ que são as suas características e
+    _____________ que permitem a inserção de código-fonte associado à
+    ocorrência de ____________________________.
+*}
+
 unit Unit1;
 
 {$mode objfpc}{$H+}
