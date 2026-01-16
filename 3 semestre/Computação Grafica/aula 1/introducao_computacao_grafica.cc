71078df847282450effc55528509986243cb7dd2
Lucas Andre Ferreira De Freitas <lucasfreitas@caesb.df.gov.br>
2026-01-16 13:53:15 -0300

    feat: adiciona arquivo C++

---
+++ 3 semestre/Computação Grafica/aula 1/introducao_computacao_grafica.cc
@@ -0,0 +1,47 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Falha ao inicializar GLFW" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Introdução à Computação Gráfica", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Falha ao criar janela" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Falha ao inicializar GLEW" << std::endl;
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+            glColor3f(1.0f, 0.0f, 0.0f); 
+            glVertex2f(-0.5f, -0.5f);
+
+            glColor3f(0.0f, 1.0f, 0.0f); 
+            glVertex2f(0.5f, -0.5f);
+
+            glColor3f(0.0f, 0.0f, 1.0f); 
+            glVertex2f(0.0f, 0.5f);
+        glEnd();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
