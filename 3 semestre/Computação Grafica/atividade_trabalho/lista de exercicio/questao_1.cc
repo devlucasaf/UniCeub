5990a0f1782ae4c41cdc747cea8ecefdad3796dc
Lucas Andre Ferreira De Freitas <lucasfreitas@caesb.df.gov.br>
2026-01-19 17:11:33 -0300

    feat: adiciona arquivos C++

---
+++ 3 semestre/Computação Grafica/atividade_trabalho/lista de exercicio/questao_1.cc
@@ -0,0 +1,55 @@
+#include <GL/glut.h>
+
+void desenho()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_POLYGON);
+        glColor3f(237.0 / 256.0, 28.0 / 256.0, 36.0 / 256.0);
+        glVertex3f(-5.0, -2.0, 0.0);
+        glVertex3f(-3.0, -4.0, 0.0);
+        glVertex3f(3.0, -4.0, 0.0);
+        glVertex3f(5.0, -2.0, 0.0);
+    glEnd();
+
+    glBegin(GL_TRIANGLES);
+        glColor3f(195.0 / 256.0, 195.0 / 256.0, 195.0 / 256.0);
+        glVertex3f(-3.0, -0.5, 0.0);
+        glVertex3f(3.0, -0.5, 0.0);
+        glVertex3f(-1.0, 4.0, 0.0);
+    glEnd();
+
+    glBegin(GL_QUADS);
+        glColor3f(128.0 / 256.0, 64.0 / 256.0, 0.0);
+        glVertex3f(-1.2, 4.0, 0.0);
+        glVertex3f(-1.2, -2.0, 0.0);
+        glVertex3f(-0.8, -2.0, 0.0);
+        glVertex3f(-0.8, 4.0, 0.0);
+    glEnd();
+
+    glBegin(GL_QUADS);
+        glColor3f(63.0 / 256.0, 72.0 / 256.0, 204.0 / 256.0);
+        glVertex3f(-1.2, 4.5, 0.0);
+        glVertex3f(0.0, 4.5, 0.0);
+        glVertex3f(0.0, 4.0, 0.0);
+        glVertex3f(-1.2, 4.0, 0.0);
+    glEnd();
+
+    glutSwapBuffers();
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(600, 600);
+    glutInitWindowPosition(345, 20);
+    glutCreateWindow("Barco");
+    glutDisplayFunc(desenho);
+
+    gluOrtho2D(-10, 10, -10, 10);
+    glClearColor(255.0/256.0, 244.0/256.0, 208.0/256.0, 1.0);
+
+    glutMainLoop();
+    return 0;
+}
---
+++ 3 semestre/Computação Grafica/atividade_trabalho/lista de exercicio/questao_2.cc
@@ -0,0 +1,79 @@
+#include <GL/glut.h>
+#include <math.h>
+
+void desenho()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    float raio = 4.0;
+    int num_segmentos = 50;
+
+    glBegin(GL_TRIANGLE_FAN);
+        glColor3f(0.0/256.0, 0.0/256.0, 51.0/256.0);
+        glVertex2f(0.0, 0.0);
+        for (int i = 0; i <= num_segmentos; ++i) {
+            float angulo = i * 2.0f * 3.14159f / num_segmentos;
+            float x = raio * cos(angulo);
+            float y = raio * sin(angulo);
+            glVertex2f(x, y);
+        }
+    glEnd();
+
+    raio = 3.0;
+    num_segmentos = 50;
+
+    glBegin(GL_TRIANGLE_FAN);
+        glColor3f(0.0/256.0, 0.0/256.0, 153.0/256.0);
+        glVertex2f(0.0, 0.0);
+        for (int i = 0; i <= num_segmentos; ++i) {
+            float angulo = i * 2.0f * 3.14159f / num_segmentos;
+            float x = raio * cos(angulo);
+            float y = raio * sin(angulo);
+            glVertex2f(x, y);
+        }
+    glEnd();
+
+    raio = 2.0;
+    num_segmentos = 50;
+
+    glBegin(GL_TRIANGLE_FAN);
+        glColor3f(0.0/256.0, 0.0/256.0, 51.0/256.0);
+        glVertex2f(0.0, 0.0);
+        for (int i = 0; i <= num_segmentos; ++i) {
+            float angulo = i * 2.0f * 3.14159f / num_segmentos;
+            float x = raio * cos(angulo);
+            float y = raio * sin(angulo);
+            glVertex2f(x, y);
+        }
+    glEnd();
+
+    raio = 1.0;
+    num_segmentos = 50;
+
+    glBegin(GL_TRIANGLE_FAN);
+        glColor3f(1.0, 1.0, 1.0);
+        glVertex2f(0.0, 0.0);
+        for (int i = 0; i <= num_segmentos; ++i) {
+            float angulo = i * 2.0f * 3.14159f / num_segmentos;
+            float x = raio * cos(angulo);
+            float y = raio * sin(angulo);
+            glVertex2f(x, y);
+        }
+    glEnd();
+
+    glFlush();
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+    glutInitWindowSize(600, 600);
+    glutInitWindowPosition(345, 20);
+    glutCreateWindow("mama");
+    glutDisplayFunc(desenho);
+    gluOrtho2D(-7, 7, -7, 7);
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glutMainLoop();
+    return 0;
+}
