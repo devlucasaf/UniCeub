e45ef178e28ebd9eca86ac01d529a134d29507ee
Lucas Andre Ferreira De Freitas <lucasfreitas@caesb.df.gov.br>
2026-03-11 13:21:56 -0300

    adicionando arquivos da aula de PDM

---
+++ 5 semestre/Programacao para Dispositivos Moveis/03_marco/aula 11-03-26/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.saudacao;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvSaudacao;
+    EditText edtNome;
+    Button btnRealizarSaudacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        tvSaudacao = (TextView) findViewById(R.id.lblSaudacao);
+        edtNome = (EditText) findViewById(R.id.txtNome);
+        btnRealizarSaudacao = (Button) findViewById(R.id.cmdSaudar);
+
+        btnRealizarSaudacao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nome = edtNome.getText().toString();
+                String saudacao = "Olá "+nome+", seja bem-vindo ao curso de Programação para Dispositivos Móveis";
+                
+                tvSaudacao.setText(saudacao);
+            }
+        });
+    }
+}
