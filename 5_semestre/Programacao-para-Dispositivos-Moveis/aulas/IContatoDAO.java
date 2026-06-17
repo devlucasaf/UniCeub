d71c87d2b5f82eb994bc2b3a564b79ec6b6f8ff4
Lucas Freitas <lucas.a.freitas@sempreceub.com>
2026-06-17 10:52:56 -0300

    Add files via upload

---
+++ 5_semestre/Programacao-para-Dispositivos-Moveis/aulas/IContatoDAO.java
@@ -0,0 +1,12 @@
+package com.example.contatosdb.persistencia;
+
+import com.example.contatosdb.model.Contato;
+
+import java.util.List;
+
+public interface IContatoDAO {
+    public boolean          salvar(Contato contato);
+    public boolean          atualizar(Contato contato);
+    public boolean          excluir(Contato contato);
+    public List<Contato>    listar();
+}
