9fe162ea1200b6805b244b77186c8a7baefff467
Lucas Freitas <lucas.a.freitas@sempreceub.com>
2026-06-17 09:28:19 -0300

    Add files via upload

---
+++ 5_semestre/Programacao-para-Dispositivos-Moveis/aulas/06_junho/aula_10-06-26/AdapterContato.java
@@ -0,0 +1,55 @@
+package com.example.contatosdb.adapter;
+
+import android.content.Context;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.contatosdb.R;
+import com.example.contatosdb.model.Contato;
+
+import java.util.List;
+
+public class AdapterContato extends RecyclerView.Adapter<AdapterContato.MyViewHolder> {
+
+    private List<Contato>   listaContatos;
+    private Context         context;
+
+    public AdapterContato(List<Contato> listaContatos) {
+        this.listaContatos = listaContatos;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemLista = (View) LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_contato, parent, false);
+        return new MyViewHolder(itemLista);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Contato contato = listaContatos.get(position);
+        holder.nomeContato.setText(contato.getNomeContato());
+        holder.telContato.setText(contato.getTelefoneContato());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaContatos.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView nomeContato, telContato;
+
+        public MyViewHolder(@NonNull View itemView){
+            super(itemView);
+            nomeContato = itemView.findViewById(R.id.textViewNome);
+            telContato = itemView.findViewById(R.id.textViewTelefone);
+        }
+    }
+}
